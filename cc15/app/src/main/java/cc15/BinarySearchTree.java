1100ea2077437162ea08bb685aa440077ef2d9bd
ElayyanAreej <areej.hossein@gmail.com>
2021-10-31 02:13:28 +0200

    cc15

---
+++ cc15/app/src/main/java/cc15/BinarySearchTree.java
@@ -0,0 +1,41 @@
+package cc15;
+
+public class BinarySearchTree {
+    Node root;
+
+    public void add(int value) {
+        root = addRecursive(root, value);
+    }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            return new Node(value);
+        }
+
+        if (value < (int)current.value) {
+            current.left = addRecursive(current.left, value);
+        } else if (value > (int)current.value) {
+            current.right = addRecursive(current.right, value);
+        } else {
+            return current;
+        }
+
+        return current;
+    }
+
+    public boolean Contains(Node current, int value) {
+        if (current == null) {
+            return false;
+        }
+        if (value == (int)current.value) {
+            return true;
+        }
+        return value < (int)current.value
+                ? Contains(current.left, value)
+                : Contains(current.right, value);
+    }
+
+    public boolean containsNode(int value) {
+        return Contains(root, value);
+    }
+}
---
+++ cc15/app/src/main/java/cc15/BinaryTree.java
@@ -0,0 +1,14 @@
+package cc15;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class BinaryTree {
+//    pre order
+//    public ArrayList preOreder( tree){
+//
+//    }
+//    in order
+//    post order which returns an array of the values, ordered appropriately.
+//
+}
